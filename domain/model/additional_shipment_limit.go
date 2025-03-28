69a8324f2d69fab7ebdbf32a6dca6688d135df69
hiroya iwama <29530882+hiwm0126@users.noreply.github.com>
2025-03-29 02:57:36 +0900

    fifth commit

--- domain/model/additional_shipment_limit.go
+++ domain/model/additional_shipment_limit.go
@@ -1,1 +1,22 @@
 package model
+
+import (
+	"github.com/hiwm0126/internship_27_test/constants"
+	"time"
+)
+
+type AdditionalShipmentLimit struct {
+	Quantity int
+	From     time.Time
+	To       time.Time
+}
+
+func NewAdditionalShipmentLimit(quantity int, from string, to string) *AdditionalShipmentLimit {
+	fromTime, _ := time.Parse(constants.DateFormat, from)
+	toTime, _ := time.Parse(constants.DateFormat, to)
+	return &AdditionalShipmentLimit{
+		Quantity: quantity,
+		From:     fromTime,
+		To:       toTime,
+	}
+}
--- domain/model/shipment_limit.go
+++ domain/model/shipment_limit.go
@@ -47,19 +47,3 @@ func (s *ShipmentLimit) CanShipping(currentPlannedShippingQuantity int, addition
 	}
 	return currentPlannedShippingQuantity+additionalQuantity <= limitQuantity
 }
-
-type AdditionalShipmentLimit struct {
-	Quantity int
-	From     time.Time
-	To       time.Time
-}
-
-func NewAdditionalShipmentLimit(quantity int, from string, to string) *AdditionalShipmentLimit {
-	fromTime, _ := time.Parse(constants.DateFormat, from)
-	toTime, _ := time.Parse(constants.DateFormat, to)
-	return &AdditionalShipmentLimit{
-		Quantity: quantity,
-		From:     fromTime,
-		To:       toTime,
-	}
-}
